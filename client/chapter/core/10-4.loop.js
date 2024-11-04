4176c5d7fb70794ded1a018b764f254d30715e75
Littlestar0508 <rist9860@gmail.com>
2024-11-04 16:16:55 +0900

    for in 문법과 hasOwnProperty와 hasOwn 문법

---
+++ client/chapter/core/10-4.loop.js
@@ -0,0 +1,29 @@
+/* ---------------- */
+/* For In Loop      */
+/* ---------------- */
+
+const javaScript = {
+  creator: 'Brendan Eich',
+  createAt: '1995.05',
+  standardName: 'ECMAScript',
+  currentVersion: 2024,
+  hasOwnProperty() {
+    return '안녕!';
+  },
+};
+
+console.log();
+
+// 객체의 속성(property) 포함 여부 확인 방법
+// - 모든 객체가 사용 가능하도록 속성이 확장되었을 때 포함 여부 결과는?
+
+// 객체 자신의 속성인지 확인하는 방법
+// - "자신(own)의 속성(property을 가지고(has)있는지 확인 방법"이 덮어쓰여질 수 있는 위험에 대처하는 안전한 방법은?
+console.log(javaScript.hasOwnProperty('creator')); // 이는 javaScript내부에 있는 hasOwnProperty함수가 실행된다
+console.log(Object.prototype.hasOwnProperty.call('javaScript', 'toString()'));
+
+console.log(Object.hasOwn('creator'));
+
+// for ~ in 문
+// - 객체 자신의 속성만 순환하려면?
+// - 배열 객체 순환에 사용할 경우?
