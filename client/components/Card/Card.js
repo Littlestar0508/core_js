4fc506bc048a0d9f90fc2e224b7680f07fb01667
Littlestar0508 <rist9860@gmail.com>
2024-11-27 15:12:59 +0900

    카드 컴포넌트 생성 및 속성 부여

--- client/components/Card/Card.js
+++ client/components/Card/Card.js
@@ -4,20 +4,51 @@ export class Card extends HTMLElement {
 
     this.attachShadow({ mode: 'open' });
 
+    this.state = {
+      showImage: this.getAttribute('show-image') || 'true',
+      title: this.getAttribute('title') || 'Card Title',
+    };
+
     this.render();
   }
 
+  static get observedAttributes() {
+    return ['show-image', 'title'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'show-image') {
+      this.state.showImage = newValue;
+
+      this.render();
+    }
+
+    if (name === 'title') {
+      this.state.title = newValue;
+
+      this.render();
+    }
+  }
+
   render() {
+    const { showImage, title } = this.state;
+
+    const figure = /* HTML */ `<figure>
+      <img src="./assets/visual.jpg" alt="" />
+      <figcaption class="a11y-hidden">눈이 소복히 언덕 위 오두막</figcaption>
+    </figure>`;
+
     this.shadowRoot.innerHTML = `
     <style>@import url('./components/Card/Card.css');</style>
     <div class='card'>
-      <figure>
-        <img src="./assets/visual.jpg" alt="" />
-        <figcaption class = 'a11y-hidden'>눈이 소복히 언덕 위 오두막</figcaption>
-      </figure>
-      <h2>Card Title</h2>
-      <p>description...</p>
-    </div>   
+      ${showImage === 'true' ? figure : ''}
+      <h2>${title}</h2>
+      
+      <p>
+        <slot name='content'>description...</slot>
+      </p>
+
+    </div>
     `;
   }
 }
