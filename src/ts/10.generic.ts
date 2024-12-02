9ab0b60d2cdc0062a43fbb2ac6983b4d2f6b17dc
Littlestar0508 <rist9860@gmail.com>
2024-12-02 16:21:09 +0900

    generic type의 기본 예시

---
+++ src/ts/10.generic.ts
@@ -0,0 +1,33 @@
+// Generic Type(중요)
+
+function sumFunc<T>(value: T): T {
+  return value;
+}
+
+const a = sumFunc(10);
+const b = sumFunc("hello");
+
+function swapAtoB<T, U>(a: T, b: U): (T | U)[] {
+  return [b, a];
+}
+
+swapAtoB(1, true);
+
+function getLength<T extends { length: number }>(data: T): number {
+  return data.length;
+}
+
+const b1 = getLength([1, 2, 3]); // 3
+const b2 = getLength("hi"); // 2
+const b3 = getLength({ name: "tiger", length: 3 }); // 2
+
+/*
+
+T:Type
+U:Unknown or Unique
+K:Key
+V:Value
+E:Element
+R:Return
+
+*/
